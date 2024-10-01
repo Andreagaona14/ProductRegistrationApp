87a94457af246c59f20a0da3cd2162a84bf230b2
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-01 16:34:24 -0500

    Se define el paquete en el que se encuentra esta clase, importación de las clases para la interfaz, declara la clase y la extiende

--- src/com/inventario/model/ProductTableModel.java
+++ src/com/inventario/model/ProductTableModel.java
@@ -1,4 +1,20 @@
 package com.inventario.model;
+//En la linea 1 se define el paquete en el que se encuentra esta clase
 
-public class ProductTableModel {
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+import java.util.ArrayList;
+//Importación de las clases para la interfaz
+
+public class ProductTableModel extends AbstractTableModel {
+    //Declara la clase y la extiende, esto significa que esta clase es una ventana de la interfaz gráfica
+    private final List<Product> productList;
+    private final String[] columnNames = {"ID", "Nombre", "Precio", "Cantidad"};
+
+    public ProductTableModel() {
+        this.productList = new ArrayList<>();
+    }
+
+    // Implementa los métodos requeridos (getRowCount, getColumnCount, getValueAt)
+    // Agrega el método addProduct
 }
