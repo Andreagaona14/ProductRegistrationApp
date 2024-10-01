1fb782c645e0cef07c517137546e3efa4a8a9e98
Andreagaona14 <andreitagaona05@gmail.com>
2024-10-01 15:50:02 -0500

    Dentro del archivo product se realizo la definición del paquete, la creación de la clase con sus atributos y el constructor

--- src/com/inventario/model/Product.java
+++ src/com/inventario/model/Product.java
@@ -1,4 +1,24 @@
 package com.inventario.model;
+//En la linea 1 se define el paquete en el que se encuentra esta clase
+
 
 public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+//En la linea 5 se crea la clase product, en la linea del 6 al 9 se definen los atributos con sus tipos
+
+
+    public Product(int id, String name, double price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+// En la linea 13 a la 17 se crea el constructor
+
+    
+    // Genera getters y setters para todos los campos
+
 }
